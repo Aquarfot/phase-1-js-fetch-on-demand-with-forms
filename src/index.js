22ea3f7be5b17ab6bc05dae98d17822ad2c3ccfb
Andrew Quarfot <andrew.quarfot@gmail.com>
2022-05-23 17:02:32 -0600

    commit tree 1429c2386328ef100510507620efdce13299f47d

--- src/index.js
+++ src/index.js
@@ -1,5 +1,19 @@
 const init = () => {
+  let inputForm = document.querySelector("form")
+  inputForm.addEventListener('submit', (event) => {
+    inputForm.preventDefault()
+    const input = document.querySelector('input#searchByID')
+
+    fetch(`http://localhost:3000/movies/${input.value}`)
+    .then(response => response.json())
+    .then(data => {
+      const title = document.querySelector('section#movieDetails h4')
+      const summary = document.querySelector('section#movieDetails p')
   
+      title.innerText = data.title
+      summary.innerText = data.summary
+    })
+    })
 }
 
 document.addEventListener('DOMContentLoaded', init);
